83ccba09b03acc77b40b35cbfa46e10c87fef236
Aline <alinesanqueza@gmail.com>
2020-07-14 12:40:11 -0300

    aspas simples e ponto e vírgula

--- src/index.js
+++ src/index.js
@@ -1,41 +1,41 @@
 import cipher from './cipher.js'
 
-document.getElementById("encode").addEventListener("click", function (event) { 
+document.getElementById('encode').addEventListener('click', function (event) { 
     event.preventDefault();
-    let offset = document.getElementById("key").value
-    let string = document.getElementById("msg").value.toUpperCase()
-    let saida = cipher.encode(offset, string)
+    let offset = document.getElementById('key').value;
+    let string = document.getElementById('msg').value.toUpperCase();
+    let saida = cipher.encode(offset, string);
 
-    if (string == ""){ 
-        alert("Digite a mensagem")
-    }else if (offset == 0){
-        alert("Deslocamento inválido")
-    }else{
-        document.getElementById("textoCifrado").value = saida
+    if (string == '') { 
+        alert('Digite a mensagem');
+    } else if (offset == 0) {
+        alert('Deslocamento inválido');
+    } else {
+        document.getElementById('encoded-text').value = saida;
     }
-})   
+});
 
-document.getElementById("decode").addEventListener("click", function (event){     
-    event.preventDefault()
-    let offset = document.getElementById("key").value
-    let string = document.getElementById("msg").value.toUpperCase()
-    let saida = cipher.decode(offset, string)
+document.getElementById('decode').addEventListener('click', function (event) {     
+    event.preventDefault();
+    let offset = document.getElementById('key').value;
+    let string = document.getElementById('msg').value.toUpperCase();
+    let saida = cipher.decode(offset, string);
 
-    if (string == ""){ 
-        alert("Digite a mensagem")
-    }else if (offset == 0){
-        alert("Deslocamento inválido")
-    }else{
-        document.getElementById("textoCifrado").value= saida
+    if (string == '') { 
+        alert('Digite a mensagem')
+    } else if (offset == 0) {
+        alert('Deslocamento inválido')
+    } else {
+        document.getElementById('encoded-text').value = saida
     }
-})
+});
 
-document.getElementById("clear").addEventListener("click", function (event){
-        event.preventDefault()
-        let offset = document.getElementById("key")
-        let string = document.getElementById("msg")
-        let saida = document.getElementById("textoCifrado")
-        offset.value = ""
-        string.value = ""
-        saida.value = ""
-    })
+document.getElementById('clear').addEventListener('click', function (event) {
+    event.preventDefault();
+    let offset = document.getElementById('key');
+    let string = document.getElementById('msg');
+    let saida = document.getElementById('encoded-text');
+    offset.value = '';
+    string.value = '';
+    saida.value = '';
+    });
